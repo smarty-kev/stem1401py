c1277104a217615c5d7bdea31f1eee495ada76dd
smarty-kev <kevinliuchen7@gmail.com>
2021-09-22 20:31:58 -0400

    minor changes

--- py210911_python3c/day98_py210918/pop_example.py
+++ py210911_python3c/day98_py210918/pop_example.py
@@ -3,6 +3,7 @@ example of Procedural Programming
 """
 
 
+# functions
 def add(x, y):
     answer = x + y
     print(answer)
@@ -13,6 +14,7 @@ def subtract(x, y):
     print(answer)
 
 
+# variables
 initial_apples = 17
 eaten_apples = 9
 print(f"There are {subtract(initial_apples, eaten_apples)} apples left.")
