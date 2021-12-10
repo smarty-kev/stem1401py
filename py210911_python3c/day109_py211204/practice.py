c397efd76455188d97e93c557cdb3c79ac0f6b2f
smarty-kev <kevinliuchen7@gmail.com>
2021-12-10 18:15:38 -0500

    breeeeh

---
+++ py210911_python3c/day109_py211204/practice.py
@@ -0,0 +1,13 @@
+"""
+i have no life hahhaihsdiahsidhasd
+"""
+
+
+class MySuperHiddenSecrets:
+    def __init__(self):
+        self.password = "YES THIS IS SOOOOOOOO MY PASSWORD"
+        self._password = "HA SIKE IN REALITY THIS IS MY PASSWORD CHEEEEHHH"
+
+    def print_pw(self):
+        pass
+
