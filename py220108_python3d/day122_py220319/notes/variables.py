32cd3541ae35609c77e928582b877750724c35ee
smarty-kev <kevinliuchen7@gmail.com>
2022-03-26 09:08:55 -0400

    day 122

---
+++ py220108_python3d/day122_py220319/notes/variables.py
@@ -0,0 +1,20 @@
+"""
+question 1
+"""
+
+variable = 1
+variable += 1
+
+
+"""
+question
+"""
+
+char_1 = "t"
+char_2 = "T"
+char_3 = "a"
+char_4 = "A"
+
+print(char_1 == char_2)
+print(char_1 == char_3)
+# etc....
