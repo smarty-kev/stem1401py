37290e43fee1fb2060e5a88acce5150eca63efc9
smarty-kev <kevinliuchen7@gmail.com>
2022-04-24 11:28:18 -0400

    finished v1 of standard edition

---
+++ py220108_python3d/day126_py220423/myplotter/standard_edition/v1/testplotter.py
@@ -0,0 +1,14 @@
+from plotter import *
+from shapetest import *
+
+plotter = MyPlotter()
+
+# 2D
+plotter.plotShape(circle)
+plotter.plotShape(rectangle)
+plotter.plotShape(square)
+plotter.plotShape(triangle)
+
+# 3D
+plotter.plotShape(sphere)
+plotter.plotShape(cube)
