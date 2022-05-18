63b953808f652921c2d0762076e7832914e60ebc
smarty-kev <kevinliuchen7@gmail.com>
2022-05-17 21:04:08 -0400

    v2

---
+++ py220108_python3d/day129_py220514/myplotter/pro_edition/v2/plotter.py
@@ -0,0 +1,103 @@
+"""
+pro edition
+
+Requirements of Pro Edition:
+An end-user may input a name of shape from their keyboard.
+The application then draws a shape that the user desires.
+The basic parameter(s) of a shape should print on the console, which determines possible measurements of a shape.
+i.e. perimeter, area, surface area, volume, etc.
+The perimeter of a 2D shape should print on the console.
+The area of a 2D shape should print on the console.
+The volume of a 3D shape should print on the console.
+The surface area of a 3D shape should print on the console.
+
+# different from standard
+It allows users to fill with a specified color.
+"""
+
+from shape_entities import *
+
+
+class PerimeterFinder:
+    def getPerimeter(self, shape_obj):
+        perimeter = shape_obj.findPerimeter()
+        return perimeter
+
+    def getMultiplePerimeter(self, *shape_obj):
+        perimeters = []
+        for obj in shape_obj:
+            perimeters.append(self.getPerimeter(obj))
+        return perimeters
+
+
+class AreaFinder:
+    def getArea(self, shape_obj):
+        area = shape_obj.findArea()
+        return area
+
+    def getMultipleArea(self, *shape_obj):
+        areas = []
+        for obj in shape_obj:
+            areas.append(self.getArea(obj))
+        return areas
+
+
+class VolumeFinder:
+    def getVolume(self, shape_obj):
+        volume = shape_obj.findVolume()
+        return volume
+
+    def getMultipleVolume(self, *shape_obj):
+        volumes = []
+        for obj in shape_obj:
+            volumes.append(self.getVolume(obj))
+        return volumes
+
+
+class Drawer:
+    def startDrawing(self, shape_obj):
+        print("===New Shape===")
+        print(f"Drawing the shape \"{shape_obj.shape_type}\"...")
+
+    def finishDrawing(self):
+        print("Finished drawing.")
+        print("\n")
+
+
+class Colorer:
+    def fillColor(self, shape_obj, color):
+        print("\n===Coloring===")
+        print(f"Coloring \"{shape_obj.shape_type}\" with the color \"{color}\".")
+        shape_obj.color = color
+        print(f"\"{shape_obj.shape_type}\" is now \"{color}\".\n")
+
+
+class MyPlotter(PerimeterFinder, AreaFinder, VolumeFinder):
+    def __init__(self):
+        self.drawer = Drawer()
+        self.colorer = Colorer()
+        self.myshapes = []
+        print("===MY PLOTTER===")
+        print("Professional Version")
+        print("IMPORTANT!!!")
+        print("PI (π) will be used with 14 digits after the comma.")
+        print("Square roots will not be taken as absolute value.")
+        print("\n")
+
+    def plotShape(self, shape_obj):
+        self.myshapes.append(shape_obj)
+        self.drawer.startDrawing(shape_obj)
+        if shape_obj.dimension == "TwoD":
+            perimeter = self.getPerimeter(shape_obj)
+            area = self.getArea(shape_obj)
+            print(f"The perimeter of the {shape_obj.shape_type} is {perimeter}.")
+            print(f"The area of the {shape_obj.shape_type} is {area}.")
+        elif shape_obj.dimension == "ThreeD":
+            area = self.getArea(shape_obj)
+            volume = self.getVolume(shape_obj)
+            print(f"The area of the {shape_obj.shape_type} is {area}.")
+            print(f"The volume of the {shape_obj.shape_type} is {volume}.")
+        self.drawer.finishDrawing()
+
+    def fillColor(self, shape_obj, color):
+        self.colorer.fillColor(shape_obj, color)
---
+++ py220108_python3d/day129_py220514/myplotter/standard_edition/v2/client_app.py
@@ -0,0 +1,105 @@
+"""
+client app
+user interface = cml
+"""
+
+from plotter import *
+
+
+# shape name
+two_d_shapes = ["Circle", "Rectangle", "Square", "Triangle"]
+three_d_shapes = ["Cube", "Sphere"]
+
+
+# main menu:
+main_menu = "Main Menu\n" \
+            "Plot Shape : [1]\n" \
+            "Exit       : [exit]\n"
+
+
+# plot shape menu
+plot_shape_menu = "Shape Menu\n"
+
+
+plot_shape_menu += "2D Shapes:\n"
+key = 1
+for shape in two_d_shapes:
+    plot_shape_menu += shape
+    plot_shape_menu += f" : [{key}]\n"
+    key += 1
+
+plot_shape_menu += "3D Shapes:\n"
+for shape in three_d_shapes:
+    plot_shape_menu += shape
+    plot_shape_menu += f" : [{key}]\n"
+    key += 1
+
+plot_shape_menu += "Return to main menu : [exit]"
+
+
+def main():
+    my_plotter = MyPlotter()
+    while True:
+        # 2d menu
+        print(main_menu)
+        main_menu_input = input("<<here>> : ")
+        if main_menu_input == "exit":
+            print("Exiting Plotter...")
+            break
+        if main_menu_input == "1":
+            while True:
+                print(plot_shape_menu)
+                shape_menu_input = input("<<here>> : ")
+                if shape_menu_input == "1":
+                    print("Circle Parameters : radius")
+                    circle = Circle(input("radius : "))
+
+                    print("\n")
+                    my_plotter.plotShape(circle)
+
+                if shape_menu_input == "2":
+                    print("Rectangle Parameters : a, b")
+                    rectangle = Rectangle(input("a : "),
+                                          input("b : "))
+
+                    print("\n")
+                    my_plotter.plotShape(rectangle)
+
+                if shape_menu_input == "3":
+                    print("Square Parameters : a")
+                    square = Square(input("a : "))
+
+                    print("\n")
+                    my_plotter.plotShape(square)
+
+                if shape_menu_input == "4":
+                    print("Triangle Parameters : base, h, a, b")
+                    triangle = Triangle(input("base : "),
+                                        input("h : "),
+                                        input("a : "),
+                                        input("b : "))
+
+                    print("\n")
+                    my_plotter.plotShape(triangle)
+
+                if shape_menu_input == "5":
+                    print("Cube Parameters : a")
+                    cube = Cube(input("a : "))
+
+                    print("\n")
+                    my_plotter.plotShape(cube)
+
+                if shape_menu_input == "6":
+                    print("Sphere Parameters : radius")
+                    sphere = Sphere(input("radius : "))
+
+                    print("\n")
+                    my_plotter.plotShape(sphere)
+
+                if shape_menu_input == "exit":
+                    break
+
+
+# main program
+if __name__ == "__main__":
+    main()
